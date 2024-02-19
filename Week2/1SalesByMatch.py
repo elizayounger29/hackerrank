0cc45d9e2062e077c3d67c0a28cd4424452fe9df
elizayounger29 <eyoungergithub@hotmail.com>
2024-02-19 11:16:46 +0000

    first exercise of week 2

--- Week2/1SalesByMatch.py
+++ Week2/1SalesByMatch.py
@@ -1,17 +1,24 @@
 from collections import Counter
 
-def sockMerchant(ar):
+def sockMerchant(n, ar):
     pairs = 0
     socks = Counter(ar)
     for sock, value in socks.items():
         if value % 2 == 0:
             pairs += value / 2
-        if value > 2:
+        elif value > 2:
             pairs += (value - 1) / 2
 
     return int(pairs)
 
 if __name__ == "__main__":
-    sock_array = [1, 2, 1, 2, 1, 3, 2]
-    result = sockMerchant(sock_array)
+    # array_length = 7
+    # sock_array = [1, 2, 1, 2, 1, 3, 2]
+    array_length = 9
+    sock_array = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+    result = sockMerchant(array_length, sock_array)
     print(result)
+
+
+    # 9
+    # 10 20 20 10 10 30 50 10 20
